4e17fa2751e2adee7eb80a93983f6c6f9b27125f
Alexey <AlexeyStadnikov@users.noreply.github.com>
2020-09-19 00:53:42 +0300

    Add `build` folder to the repository to provide a live demo. Add link to Live Demo to README.md

---
+++ build/precache-manifest.1ae5b12bdc7f187fac1bf17c5a425128.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6709110d309428a6dec43fd86c43932",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad218d2846a064a91bb1",
+    "url": "/static/css/main.9fca35eb.chunk.css"
+  },
+  {
+    "revision": "e8b9ce75183533f41960",
+    "url": "/static/js/2.82e4d9e2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.82e4d9e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad218d2846a064a91bb1",
+    "url": "/static/js/main.5bb27af1.chunk.js"
+  },
+  {
+    "revision": "3d5d07b6d748ac1c8db3",
+    "url": "/static/js/runtime-main.45127e9e.js"
+  }
+]);
